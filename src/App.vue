dc651163c0a4be6316fdea78727531779ace03f3
taoyage <tougakaku@gmail.com>
2017-12-09 16:11:59 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="scss">
+  @import "common/styles/variable";
+
+  #app {
+    color:$color-link;
+  }
+</style>
